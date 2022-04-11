de4cd56e301543b1ecdf42cd44fb0f9dfd9b02ce
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-04-11 10:43:03 +0530

    [ADD] purchase_tag

---
+++ purchase_tag/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from . import purchase_tag
+from . import purchase_order
